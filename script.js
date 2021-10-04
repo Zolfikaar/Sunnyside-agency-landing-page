7db51e38b9fddbc2d8c053eb5eb4c64cd65add71
Zolfikaar <fukar.work@gmail.com>
2021-10-04 18:55:32 +0300

    corecting basic structure (like button,ul,a where they needed) and style

--- script.js
+++ script.js
@@ -17,6 +17,7 @@ window.addEventListener("resize", () => {
 // Toggle navbar box on
 burgerBtn.addEventListener('click', () => {
   navs.classList.toggle(navHolderClass);
+  burgerBtn.setAttribute('aria-expanded','ture')
 })
 
 
@@ -30,34 +31,3 @@ activeNav.forEach(nav => {
   })
 });
 
-
-
-const siblings = (elem,filter) => {
-  // create an empty array
-  let siblings = [];
-
-  // if no parent, return empty list
-  if (!elem.parentNode) {
-      return siblings;
-  }
-
-  // first child of the parent node
-  let sibling = elem.parentNode.firstElementChild;
-
-  // loop through next siblings until `null`
-  do {
-      // push sibling to array
-      if (sibling != elem && (!filter || filter(sibling))) {
-          siblings.push(sibling);
-      }
-  } while (sibling = sibling.nextElementSibling);
-  
-  return siblings;
-};
-
-const drink = document.querySelector('#drink');
-
-// get all all siblings
-const nodes = siblings(drink);
-
-nodes.forEach(li => console.log(li.innerText));
